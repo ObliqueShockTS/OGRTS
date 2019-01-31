5ac7555fc3b03a223717410f53cdec840381ead9
Tianshu Zhang <zts.gps@gmail.com>
2019-01-31 13:38:41 -0500

    0131Ver

--- test/test/test.f90
+++ test/test/test.f90
@@ -1,3 +1,6 @@
+!This is a 3D ray tracing code for nolinear acoustic propagation
+!developed by Tianshu Zhang, TFDTG, University of Florida
+
 program test
     use bspline_kinds_module, only: wp
     implicit none
@@ -20,12 +23,12 @@ program test
     real(wp)  ::  soundspeed, Uwindspeed, Vwindspeed
     real(wp)  ::  xp,yp,zp,xm,ym,zm,Cup,Cdown,Uup,Udown,Vup,Vdown,xt1,yt1,zt1
 
-    nx=1000
-    ny=20
-    nz=1000
-    nt=50
-    dt=0.8_wp
-    omega=15.
+    nx = 1000
+    ny = 20
+    nz = 1000
+    nt = 50
+    dt = 0.8_wp
+    omega = 15.
     allocate (x(nx,ny,nz))
     allocate (y(nx,ny,nz))
     allocate (z(nx,ny,nz))
@@ -91,158 +94,156 @@ program test
     call cellcenter(x,y,z,xc,yc,zc,nx,ny,nz)
     open(unit = 3, file ='DataXYZ.txt',form='formatted')
     
-do NTT=1,nt
-    omega=30.
-    d1 = 1000000._wp
-    ii = 0
-    jj = 0
-    kk = 0
+    do NTT=1,nt
+        omega=30.
+        d1 = 1000000._wp
+        ii = 0
+        jj = 0
+        kk = 0
     
-    !boundary condition judgement
-    If (Zver<(200._wp)) then
-        Zver=400.-abs(Zver)
-        Nnew(3)=-Nnew(3)
-    endif
-    write(3,*) xver, yver, zver
-    do i = 1,nx-1
-        do j = 1,ny-1
-            do,k = 1,nz-1
-                d = dabs((xver-xc(i,j,k))**2+(yver-yc(i,j,k))**2+(zver-zc(i,j,k))**2)
-                if (d<d1) then
-                    d1 = d
-                    ii = i
-                    jj = j
-                    kk = k
-                    !print*, KK
-                endif
+        !boundary condition judgement
+        If (Zver<(200._wp)) then
+            Zver=400.-abs(Zver)
+            Nnew(3)=-Nnew(3)
+        endif
+        write(3,*) xver, yver, zver
+        do i = 1,nx-1
+            do j = 1,ny-1
+                do,k = 1,nz-1
+                    d = dabs((xver-xc(i,j,k))**2+(yver-yc(i,j,k))**2+(zver-zc(i,j,k))**2)
+                    if (d<d1) then
+                        d1 = d
+                        ii = i
+                        jj = j
+                        kk = k
+                        !print*, KK
+                    endif
+                end do
             end do
         end do
-    end do
-    
-        
-    
-    !print*,xver
-    !print*, kk
-    do i=1,4
-        do j=1,4
-            do k=1,4
-                !print*, ii-2+i,jj-2+j,kk-2+k
-                XX(i,j,k) = x(ii-2+i,jj-2+j,kk-2+k)
-                YY(i,j,k) = y(ii-2+i,jj-2+j,kk-2+k)
-                ZZ(i,j,k) = z(ii-2+i,jj-2+j,kk-2+k)
-                !UU(i,j,k) = Uwind(ii-2+i,jj-2+j,kk-2+k)
-                !CC(i,j,k) = C0(ii-2+i,jj-2+j,kk-2+k)
-                !print*, ii-2+i,jj-2+j,kk-2+k
-            end do
-        end do
-    end do
-   
-    !calculate Uwind and C0 for 4X4X4 domian
-     do i=1,4
-        do j=1,4
-            do k=1,4
-                zt1 = zz(i,j,k) 
-                yt1 = yy(i,j,k)
-                xt1 = xx(i,j,k)
-                xt = real(xt1)
-                yt = real(yt1)
-                zt = real(zt1)
-                !call gws5(yd,-5.0,zt/1000.,(29.6516344+xt/1000./111.32),...
-                !...(-82.3248262+yt/1000./96.74),12.0,150.0,150.0,(/4.0,4.0/),w(1:2))
-                !call GTD7(yd,-5.0,zt/1000.,(29.6516344+xt/1000./111.32),...
-                !...(-82.3248262+yt/1000./96.74),12,F107A,F107,AP,MASS,D,T)
-                !call gws5(yd,-5.0,10.,29.6516344,-82.3248262,12.0,150.0,150.0,(/4.0,4.0/),w(1:2))
-                UU(i,j,k)  = Uwindspeed(xt1,yt1,zt1)!(zt-2000)**2./100000!w(2)
-                VV(i,j,k)  = Vwindspeed(xt1,yt1,zt1)!log(1.+abs(zt-2000))/100000
-                CC(i,j,k)  = Soundspeed(xt1,yt1,zt1)!343._wp-dlog(1._wp+abs(zt-2000))!340-zt/1000!C0(ii-2+i,jj-2+j,kk-2+k)
-            end do
-        end do
-     end do  
-    !write(*,*) UU, w(1), w(2), xt
-     
- 
-    do i=1,4
-        do j=1,4
-            do k=1,4
-                xp=x(ii-1+i,jj-1+j,kk+k)
-                yp=y(ii-1+i,jj-1+j,kk+k)
-                zp=z(ii-1+i,jj-1+j,kk+k)
-                xm=x(ii-1+i,jj-1+j,kk+k-2)
-                ym=y(ii-1+i,jj-1+j,kk+k-2)
-                zm=z(ii-1+i,jj-1+j,kk+k-2)
-                
-                Uup   = Uwindspeed(xp,yp,zp)
-                Udown = Uwindspeed(xm,ym,zm)
-                
-                Vup   = Vwindspeed(xp,yp,zp)
-                Vdown = Vwindspeed(xm,ym,zm)
-                
-                Cup   = Soundspeed(xp,yp,zp)
-                Cdown = Soundspeed(xm,ym,zm)
-                
-                Duz(i,j,k) = (Uup-Udown)/200.
-                Dcz(i,j,k) = (Cup-Cdown)/200.
-                Dvz(i,j,k) = (Vup-Vdown)/200.
-            end do
-        end do
-    end do
-    
-    Xnx = XX(:,1,1)
-    Yny = YY(1,:,1)
 
-    Znz = ZZ(1,1,:)
+
+        !print*,xver
+        !print*, kk
+        do i=1,4
+            do j=1,4
+                do k=1,4
+                    !print*, ii-2+i,jj-2+j,kk-2+k
+                    XX(i,j,k) = x(ii-2+i,jj-2+j,kk-2+k)
+                    YY(i,j,k) = y(ii-2+i,jj-2+j,kk-2+k)
+                    ZZ(i,j,k) = z(ii-2+i,jj-2+j,kk-2+k)
+                    !UU(i,j,k) = Uwind(ii-2+i,jj-2+j,kk-2+k)
+                    !CC(i,j,k) = C0(ii-2+i,jj-2+j,kk-2+k)
+                    !print*, ii-2+i,jj-2+j,kk-2+k
+                end do
+            end do
+        end do
     
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,CC,ValC)
-   
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,UU,ValU)
+        !calculate Uwind and C0 for 4X4X4 domian
+         do i=1,4
+            do j=1,4
+                do k=1,4
+                    zt1 = zz(i,j,k) 
+                    yt1 = yy(i,j,k)
+                    xt1 = xx(i,j,k)
+                    xt = real(xt1)
+                    yt = real(yt1)
+                    zt = real(zt1)
+                    !call gws5(yd,-5.0,zt/1000.,(29.6516344+xt/1000./111.32),...
+                    !...(-82.3248262+yt/1000./96.74),12.0,150.0,150.0,(/4.0,4.0/),w(1:2))
+                    !call GTD7(yd,-5.0,zt/1000.,(29.6516344+xt/1000./111.32),...
+                    !...(-82.3248262+yt/1000./96.74),12,F107A,F107,AP,MASS,D,T)
+                    !call gws5(yd,-5.0,10.,29.6516344,-82.3248262,12.0,150.0,150.0,(/4.0,4.0/),w(1:2))
+                    UU(i,j,k)  = Uwindspeed(xt1,yt1,zt1)!(zt-2000)**2./100000!w(2)
+                    VV(i,j,k)  = Vwindspeed(xt1,yt1,zt1)!log(1.+abs(zt-2000))/100000
+                    CC(i,j,k)  = Soundspeed(xt1,yt1,zt1)!343._wp-dlog(1._wp+abs(zt-2000))!340-zt/1000!C0(ii-2+i,jj-2+j,kk-2+k)
+                end do
+            end do
+         end do  
+        !write(*,*) UU, w(1), w(2), xt
+
+     
+        do i=1,4
+            do j=1,4
+                do k=1,4
+                    xp = x(ii-1+i,jj-1+j,kk+k)
+                    yp = y(ii-1+i,jj-1+j,kk+k)
+                    zp = z(ii-1+i,jj-1+j,kk+k)
+                    xm = x(ii-1+i,jj-1+j,kk+k-2)
+                    ym = y(ii-1+i,jj-1+j,kk+k-2)
+                    zm = z(ii-1+i,jj-1+j,kk+k-2)
+
+                    Uup   = Uwindspeed(xp,yp,zp)
+                    Udown = Uwindspeed(xm,ym,zm)
+
+                    Vup   = Vwindspeed(xp,yp,zp)
+                    Vdown = Vwindspeed(xm,ym,zm)
+
+                    Cup   = Soundspeed(xp,yp,zp)
+                    Cdown = Soundspeed(xm,ym,zm)
+
+                    Duz(i,j,k) = (Uup-Udown)/200.
+                    Dcz(i,j,k) = (Cup-Cdown)/200.
+                    Dvz(i,j,k) = (Vup-Vdown)/200.
+                end do
+            end do
+        end do
+
+        Xnx = XX(:,1,1)
+        Yny = YY(1,:,1)
+        Znz = ZZ(1,1,:)
+
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,CC,ValC)
     
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,VV,ValV)
-    
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,Duz,Valduz)
-    
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,Dvz,Valdvz)
-    
-    call interpP(Xnx,Yny,Znz,xver,yver,zver,Dcz,Valdcz)
-    !print*, Valduz
-    !**********************************************************************
-    ! start here
-    ! For Gainesville Area
-    ! 1 degree latitude 111.32 km
-    ! 1 degree longtitude 96.7421km
-    
-    !you have all the parameters to find n^bar
-    !for one time step, your finite difference method for n^bar:
-    
-    Knormal=omega/ValC
-    Kx=Knormal*Nnew(1)
-    Ky=Knormal*Nnew(2)
-    Kz=Knormal*Nnew(3)
-    
-    Term2(1) = dudx*kx+dvdx*ky+0._wp
-    Term2(2) = dudy*kx+dvdy*ky+0._wp
-    Term2(3) = Valduz*kx+Valdvz*ky+0._wp
-    
-    Term1(1) = dcdx*Knormal
-    Term1(2) = dcdy*Knormal
-    Term1(3) = Valdcz*Knormal
-    
-    !Knew = K(nt)+dt*(-Term1-Term2)
-    !print*,valduz
-    Nnew = Nnew+dt/Knormal*(-Term1-Term2-sum(Nnew*(-Term1-Term2))*Nnew)
-    Nnew(1) = Nnew(1)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
-    Nnew(2) = Nnew(2)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
-    Nnew(3) = Nnew(3)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
-    Xrecord(NTT) = xver
-    Yrecord(NTT) = yver
-    Zrecord(NTT) = zver
-    
-    xver = xver+dt*(ValC*Nnew(1)+ValU)
-    yver = yver+dt*(ValC*Nnew(2)+ValV)+0.!ValV
-    zver = zver+dt*(ValC*Nnew(3))+0.!ValW
-    print*,  zver,ValU, ValC
-    
-    
-enddo 
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,UU,ValU)
+
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,VV,ValV)
+
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,Duz,Valduz)
+
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,Dvz,Valdvz)
+
+        call interpP(Xnx,Yny,Znz,xver,yver,zver,Dcz,Valdcz)
+        !print*, Valduz
+        !**********************************************************************
+        ! start here
+        ! For Gainesville Area
+        ! 1 degree latitude 111.32 km
+        ! 1 degree longtitude 96.7421km
+
+        !you have all the parameters to find n^bar
+        !for one time step, your finite difference method for n^bar:
+
+        Knormal = omega/ValC
+        Kx = Knormal*Nnew(1)
+        Ky = Knormal*Nnew(2)
+        Kz = Knormal*Nnew(3)
+
+        Term2(1) = dudx*kx+dvdx*ky+0._wp
+        Term2(2) = dudy*kx+dvdy*ky+0._wp
+        Term2(3) = Valduz*kx+Valdvz*ky+0._wp
+
+        Term1(1) = dcdx*Knormal
+        Term1(2) = dcdy*Knormal
+        Term1(3) = Valdcz*Knormal
+
+        !Knew = K(nt)+dt*(-Term1-Term2)
+        !print*,valduz
+        Nnew = Nnew+dt/Knormal*(-Term1-Term2-sum(Nnew*(-Term1-Term2))*Nnew)
+        Nnew(1) = Nnew(1)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
+        Nnew(2) = Nnew(2)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
+        Nnew(3) = Nnew(3)/(sqrt((Nnew(1)**2+Nnew(2)**2+Nnew(3)**2)))
+        Xrecord(NTT) = xver
+        Yrecord(NTT) = yver
+        Zrecord(NTT) = zver
+
+        xver = xver+dt*(ValC*Nnew(1)+ValU)
+        yver = yver+dt*(ValC*Nnew(2)+ValV)+0.!ValV
+        zver = zver+dt*(ValC*Nnew(3))+0.!ValW
+        print*,  zver,ValU, ValC
+
+
+    enddo 
     
 
     !write(*,*) w(1), w(2), ZT , ZZ(4,4,4), (XT/1000.)
@@ -319,7 +320,7 @@ function  Vwindspeed(x,y,z)
   Vwindspeed = w(1)
   !Vwindspeed = 0.!(z-2000)**2./1000000
 
-    end function
+end function
 
 !***************************************************************************************
     
@@ -356,3 +357,5 @@ function  Vwindspeed(x,y,z)
 !    print*, W,T,DDD
 !    
 !    end program
+
+    
